21bb81ad48718f9cd5ae8a059d940ce44045cbe6
andresjacobo <andresjacobo@me.com>
2019-04-15 11:27:13 -0700

    Implemented /add_license with typical scenarios

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 from os import environ
-from datetime import datetime
+import datetime
+import time
 from functools import wraps
 from flask import request, Response
 
@@ -23,7 +24,7 @@ class License(db.Model):
     number_of_seats = db.Column(db.Integer(), nullable=False)
     expiration_date = db.Column(db.DateTime(), nullable=True)
     is_upgrade = db.Column(db.Boolean, nullable=False)
-    upgrade_from_serial = db.Column(db.String(), nullable=True)
+    upgrade_from_key = db.Column(db.String(), nullable=True)
     date = db.Column(db.DateTime(), nullable=True)
 
     def to_json_dict(self):
@@ -31,7 +32,7 @@ class License(db.Model):
             "id": self.serial,
             "product_name": self.product_name,
             "entity_id": self.entity_id,
-            "exp": "TODO!!!",
+            "exp": time.mktime(self.expiration_date.timetuple()),
             "number_of_seats": self.number_of_seats,
             "edition": {"en": "Full Edition"}
         }
@@ -63,7 +64,7 @@ def create_db():
         product_name="DOLPHINITO",
         enabled=True,
         entity_id="595959595959595-User",
-        date=datetime(year=2018,month=11,day=30)
+        date=datetime.datetime(year=2018,month=11,day=30)
     )
 
     db.session.add(license_1)
@@ -115,8 +116,88 @@ def info():
 @requires_auth
 def add_license():
     """See if a license can be added to an entity or whether we should ask more information, or whether we should
-    deny the request."""
-    pass
+    deny the request. Note that you may have additional requirements depending on your business requirements."""
+    payload_dict = request.get_json()
+    product_name = payload_dict.get("aud")
+    key = payload_dict.get("key")
+    entity_id = payload_dict.get("entityId")
+    precondition = payload_dict.get("precondition")
+
+    license = License.query().filter_by(key=key, product_name=product_name).first()
+
+
+    #Add non-existing license.
+    if license is None:
+        return Response(
+            jsonify({"description": "The license key '{}' is not a valid license".format(key)}),
+            409
+        )
+
+    #Add disabled license.
+    if not license.enabled:
+        return Response(
+            jsonify({"description": "The license key '{}' cannot be added at this time. Please contact "
+                                    "(Dolfinito®)[https://www.dolfinito.com/support] for assistance.".format(key)}),
+            409
+        )
+
+    #Add license to different entity.
+    if license.entity_id is not None and license.entity_id != entity_id:
+        return Response(
+            jsonify({"description": "The license key '{}' has already been validated by someone else. Please contact "
+                                    "(Dolfinito®)[https://www.dolfinito.com/support] for assistance.".format(key)}),
+            409
+        )
+
+
+    #Add upgrade
+    if license.is_upgrade:
+        #Missing precondition.
+        if precondition is None:
+            return Response(
+                jsonify({"description": "Please enter a previous license key to upgrade from."}),
+                428
+            )
+
+        previous_key = License.query().filter_by(key=precondition).first()
+
+        #Non-existing previous license key
+        if previous_key is None:
+            return Response(
+                jsonify({"description": "The license key '{}' is not a valid license. "
+                                        "Please enter a different license to upgrade from.".format(precondition)}),
+                412
+            )
+
+        #Previous license already upgraded
+        if license.upgrade_from_key is not None and license.upgrade_from_key != precondition:
+            return Response(
+                jsonify({"description": "The license key '{}' has already been upgraded to a different license key. Please contact "
+                                        "(Dolfinito®)[https://www.dolfinito.com/support] for assistance.".format(precondition)}),
+                412
+            )
+
+        #Previous license is a non-validated upgrade.
+        if previous_key.is_upgrade and previous_key.entity_id is None:
+            return Response(
+                jsonify({
+                            "description": "The license key '{}' cannot be used to upgrade. Please enter "
+                                           "a different license key to upgrade from.".format(precondition)}),
+                412
+            )
+
+        #Valid upgrade. We make sure to cluster all licenses.
+        licenses = [previous_key.to_json_dict(), license.to_json_dict()]
+        a_license = previous_key
+
+        while a_license.upgrade_from_key is not None:
+            a_license = License.query().filter_by(key=a_license.upgrade_from_key).first()
+            licenses.append(a_license.to_json_dict())
+    else:
+        licenses = [license.to_json_dict()]
+
+    #Add full license
+    return jsonify({"licenses": [licenses]})
 
 @app.route("/remove_license", methods=["POST"])
 @requires_auth
